9403e3298f12a347b448310fe7b776f6ff245367
Cecil Verghese <cecil.verghese@gmail.com>
2018-01-25 04:43:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+}
